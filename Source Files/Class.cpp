8c1a597c7e2e6a4cdf82459ae39fe5125aad2444
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-04-01 07:53:29 -0400

    Create Class.cpp

---
+++ Source Files/Class.cpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "Definitions.h"
+class lineType {
+public:
+	double slope();
+	void showValues(int);
+	void showQuadInfo();
+	bool equal(lineType line1, lineType line2);
+	bool parallel(lineType line1, lineType line2);
+	bool perpendicular(lineType line1, lineType line2);
+	void pointOfIntersection(lineType line1, lineType line2, double &x, double &y);
+	int quadrilateral();
+private:
+	double a, b, c;
+};
+
